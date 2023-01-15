34896bf2eed41055c19a7c6b3437a31bbef079b0
obrunofilipe <brunofilipe377@gmail.com>
2023-01-15 03:25:22 +0000

    versão 1 e 2 da implementação com cuda

--- CP_TP3/src/k_means_cuda_v2.cu
+++ CP_TP3/src/k_means_cuda_v2.cu
@@ -6,8 +6,7 @@
 #include <sys/time.h>
 #include <cuda.h>
 #include <chrono>
-
-
+#include <time.h>
 
 using namespace std;
 
@@ -35,70 +34,60 @@ __global__
 void kMeansKernel (Ponto *d_points, Centroide *d_centroids) { // código executado no GPU
     int id = blockIdx.x * blockDim.x + threadIdx.x;
     
-    Ponto point = d_points[id];
-    //d_points[id].cluster = 1;
-    
-    float d = 10000.0f;
-    int cluster = -1;
+    int N = 10000000;
+    int K = 4;
 
-    for(int j = 0; j < 4; j++){
+    if (id < N){
+        Ponto point = d_points[id];
+        //d_points[id].cluster = 1;
         
-        float tmp = (point.x - d_centroids[j].x) * (point.x- d_centroids[j].x) + 
-                    (point.y - d_centroids[j].y) * (point.y - d_centroids[j].y);
-        if (tmp < d){
-            d = tmp;
-            cluster = j;
+        float d = 10000.0f;
+        int cluster = -1;
+
+        for(int j = 0; j < 4; j++){
+            
+            float tmp = (point.x - d_centroids[j].x) * (point.x- d_centroids[j].x) + 
+                        (point.y - d_centroids[j].y) * (point.y - d_centroids[j].y);
+            if (tmp < d){
+                d = tmp;
+                cluster = j;
+            }
+        }
+
+        int cl = point.cluster; // obtençao do cluster em que estava o ponto
+
+        if(cl != cluster && cluster != -1){
+            d_points[id].cluster = cluster;
         }
     }
 
-    int cl = point.cluster; // obtençao do cluster em que estava o ponto
-
-    if(cl != cluster && cluster != -1){
-        d_points[id].cluster = cluster;
+    if(id<K){
+        d_centroids[id].soma_x = 0;
+        d_centroids[id].soma_y = 0;
+        d_centroids[id].total_pontos = 0;
     }
-}
-
-void launchKernel(Ponto *points, Centroide *centroids){
-
-    // pointer to the device memory
-    Ponto *d_points;
-    Centroide *d_centroids;
-
-
-    // allocate memory on the device
-    cudaMalloc ( &d_points, N*sizeof(Ponto));
-	cudaMalloc ( &d_centroids, K*sizeof(Centroide));
-    //checkCUDAError("mem allocation");
-
-    //copy points to device
-    cudaMemcpy(d_points, points, N*sizeof(Ponto), cudaMemcpyHostToDevice);
-    //copy centroids to device
-    cudaMemcpy(d_centroids, centroids, K*sizeof(Centroide), cudaMemcpyHostToDevice);
-    //checkCUDAError("memcpy h->d");
-
-
-    // launch the kernel
-	//startKernelTime ();
-	kMeansKernel <<< (N+512-1)/512, 512 >>> (d_points, d_centroids);
-	//stopKernelTime ();
-	//checkCUDAError("kernel invocation");
-    //cudaDeviceSynchronize();
-    // copy points output back to host
-    cudaMemcpy(points, d_points, N*sizeof(Ponto), cudaMemcpyDeviceToHost);
-    // copy centroids output back to host
-    cudaMemcpy(centroids, d_centroids, K*sizeof(Centroide), cudaMemcpyDeviceToHost);
-    //checkCUDAError("memcpy d->h");
-
-
-    // free the device memory
-    cudaFree(d_points);
-    cudaFree(d_centroids);
-    //checkCUDAError("mem free");
-
     
+    int cluster = d_points[id].cluster;
+    atomicAdd(&d_centroids[cluster].soma_x,d_points[id].x);
+    atomicAdd(&d_centroids[cluster].soma_y,d_points[id].y);
+    atomicAdd(&d_centroids[cluster].total_pontos,1);
+    
+    
+    
+}
+
+__global__
+void newCentroidsKernel (Ponto* d_points, Centroide* d_centroids){
+
+    int id = threadIdx.x;
+    
+    d_centroids[id].x = d_centroids[id].soma_x/d_centroids[id].total_pontos;
+    d_centroids[id].y = d_centroids[id].soma_y/d_centroids[id].total_pontos;
 
 
 }
+
+
 
 void init_pontos(Ponto** p, Centroide** c){
      
@@ -161,16 +150,47 @@ void printPoints(Ponto* points, Centroide* centroids, int n){
 
 int k_means(Ponto* points, Centroide* centroids){
 
-    int changed_some_point = 1;
+    //int changed_some_point = 1;
     int n_iter = 0;
+
+
+    // pointer to the device memory
+    Ponto *d_points;
+    Centroide *d_centroids;
+
+
+    // allocate memory on the device
+    cudaMalloc ( &d_points, N*sizeof(Ponto));
+	cudaMalloc ( &d_centroids, K*sizeof(Centroide));
+
+
+    //copy points to device
+    cudaMemcpy(d_points, points, N*sizeof(Ponto), cudaMemcpyHostToDevice);
+    //copy centroids to device
+    cudaMemcpy(d_centroids, centroids, K*sizeof(Centroide), cudaMemcpyHostToDevice);
+
 
     while(/*changed_some_point &&*/ n_iter < 20){// enquanto há pontos a mudarem de cluster
         //changed_some_point = 0;
-        launchKernel(points, centroids);
+
+        // launch the kernel
+	    //startKernelTime ();
+	    kMeansKernel <<< 20000, 500 >>> (d_points, d_centroids); // atribuit os pontos a centroides
+
+        newCentroidsKernel <<< 1, 4 >>> (d_points, d_centroids); //calcular os novos centroides
+
         //printPoints(points, centroids, 20);
-        new_centroids(points, centroids);
+        //new_centroids(points, centroids);
         n_iter++;
     }
+
+    cudaMemcpy(centroids, d_centroids, K*sizeof(Centroide), cudaMemcpyDeviceToHost);
+
+    // free the device memory
+    cudaFree(d_points);
+    cudaFree(d_centroids);
+
+
     return n_iter;
 }
 
@@ -190,14 +210,15 @@ int main(int argc, char** argv){
 
     clock_t start, end;
     double elapsed;
+
     start = clock();
 
     int n_iter = k_means(pontos,centroides);
-    
+
     end = clock();
     elapsed = (double)(end - start) / CLOCKS_PER_SEC;
     printf("Elapsed time: %lf seconds\n", elapsed);
-
+    
     printf("N = %d, K = %d\n", N, K);
     for(int i = 0; i < K; i++)
        printf("Center: (%f,%f) : Size: %d\n", centroides[i].x, centroides[i].y, centroides[i].total_pontos); 
